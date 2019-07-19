8a6a57b6b59dc6562ba80aeb4f3cab05e9fbc196
eliagitai <elia.ahadi@gmail.com>
2019-07-19 14:11:39 +0900

    updated app

--- app/ImageCropper.js
+++ app/ImageCropper.js
@@ -1,8 +1,8 @@
-import React, { PureComponent, useState, useEffect, useRef, useCallback } from 'react';
+import React, { Component, useState, useEffect, useRef, useCallback } from 'react';
 import { Image, ImageEditor, Dimensions } from 'react-native';
 import PropTypes from 'prop-types';
 import ImageZoom from 'react-native-image-pan-zoom';
-import {setSizes} from './helpers/setSizes';
+import setSizes from './helpers/setSizes';
 
 const window = Dimensions.get('window');
 const w = window.width;
@@ -12,56 +12,43 @@ const h = window.height;
  * @param {string, function} imageURI, mountCropperParams
  * @returns {object} cropped image
  */
-function ImageCropper(props) {
-  const imageZoom = useRef();
-  const [state, setState] =  useState(
-    {
-      positionX: 0, 
+class ImageCropper extends Component {
+  constructor(props) {
+    super(props);
+    this.imageZoom = React.createRef();
+    this.state = {
+      positionX: 0,
       positionY: 0,
       width: 0,
       height: 0,
       minScale: 1.01,
       adjustedHeight: 0,
-      loading: true
-    }
-  );
+      loading: true,
+    };
+  }
+
+  static propTypes = {
+    imageUri: PropTypes.string.isRequired,
+    mountCropperParams: PropTypes.func.isRequired,
+  };
 
   /**
    * @param none
    * @returns {object} image size
    */
-  useEffect(() => {
-    const { imageUri } = props;
-    
+  componentDidMount() {
+    const { imageUri } = this.props;
+
     /**
      * @param none
      * @returns {callback} result URL 
      */
     Image.getSize(imageUri, (width, height) => {
-      const { mountCropperParams } = props;
+      const { mountCropperParams } = this.props;
+      var [srcSize, fittedSize, scale, screenSize] = setSizes(w, h, width, height);
       
-      // calculate and set screen size
-      const screenSize = { w, h };
-      const srcSize = { w: width, h: height };
-      const fittedSize = { w: 0, h: 0 };
-      let scale = 1.01;
-  
-      if (width < height) {
-          fittedSize.h = height * w / width;
-          fittedSize.w = w;
-      }
-      else if (width > height) {
-        fittedSize.w = width * w / height;
-        fittedSize.h = w;
-      } 
-      else if (width === height) {
-        scale = 1;
-        fittedSize.w = w;
-        fittedSize.h = w;
-      }
-
       // set state updates the screen state and applies callback of screen zoom and mounts the cropper params
-      setState(
+      this.setState(
         prevState => ({
           ...prevState,
           screenSize,
@@ -71,61 +58,58 @@ function ImageCropper(props) {
           loading: false,
         }),
         () => {
-          imageZoom.current.centerOn({
+          this.imageZoom.current.centerOn({
             x: 0,
             y: 0,
             scale,
             duration: 0,
           });
-          mountCropperParams(state);
-        }
-      )
-    })
-  }, [state])
-    
+          mountCropperParams(this.state);
+        },
+      );
+    });
+  }
+
   /**
    * @param { positionX, positionY, scale } integer, integer, integer
    * @returns {callback} result URL 
    */
-  const handleTouchMove = useCallback(({ positionX, positionY, scale }) => {
-    const { mountCropperParams } = props;
-    
-    setState(
+  handleTouchMove = ({ positionX, positionY, scale }) => {
+    const { mountCropperParams } = this.props;
+
+    // set state updates the screen state and applies callback to mount the cropper params
+    this.setState(
       prevState => ({
         ...prevState,
         positionX,
         positionY,
         scale,
       }),
-      () => mountCropperParams(state),
+      () => mountCropperParams(this.state),
     );
-  }, [state]);
+  };
 
-  const { loading, fittedSize, minScale } = state;
-  const { imageUri, ...restProps } = props;
+  render() {
+    const { imageUri, ...Props } = this.props;
+    const imageSrc = { uri: imageUri };
+    const { loading, fittedSize, minScale } = this.state;
 
-  const imageSrc = { uri: props.imageUri };
-
-  return !state.loading ? (
-    <ImageZoom
-      ref={imageZoom}
-      {...restProps}
-      cropWidth={w}
-      cropHeight={w}
-      imageWidth={fittedSize.w}
-      imageHeight={fittedSize.h}
-      minScale={minScale}
-      onMove={handleTouchMove}
-    >
-      <Image style={{ width: fittedSize.w, height: fittedSize.h }} source={imageSrc} />
-    </ImageZoom>
-  ) : null;
-}
-
-//setup propTypes
-ImageCropper.propTypes = {
-  imageUri: PropTypes.string.isRequired,
-  mountCropperParams: PropTypes.func.isRequired,
+    return !this.state.loading ? (
+      <ImageZoom
+        ref={this.imageZoom}
+        {...Props}
+        cropWidth={w}
+        cropHeight={w}
+        imageWidth={fittedSize.w}
+        imageHeight={fittedSize.h}
+        minScale={minScale}
+        onMove={this.handleTouchMove}
+      >
+        <Image style={{ width: fittedSize.w, height: fittedSize.h }} source={imageSrc} />
+      </ImageZoom>
+    ) : null;
+  }
 }
 
 export default ImageCropper;
+
